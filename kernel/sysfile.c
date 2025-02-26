3651e703b245ee123412512569cf01a24ba9939a
yoonsoo <ohyoonoo12345@gmail.com>
2025-02-26 19:18:56 +0900

    2/26 19:18

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -503,3 +503,35 @@ sys_pipe(void)
   }
   return 0;
 }
+
+uint64	
+sys_mmap(void)
+{
+	uint64 addr;
+	int len;
+	int prot;
+	int flags;
+	int fd;
+	int offset;
+
+	argaddr(0, &addr);
+	argint(1, &len);
+	argint(2, &prot);
+	argint(3, &flags);
+	argint(4, &fd);
+	argint(5, &offset);
+
+	return -1;
+}
+
+uint64
+sys_munmap(void)
+{
+	uint64 addr;
+	int len;
+
+	argaddr(0, &addr);
+	argint(1, &len);
+
+	return -1;
+}
